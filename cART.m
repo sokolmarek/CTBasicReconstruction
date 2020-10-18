c6692080422ce93e28af3202aa0b80374d22d4ce
Marek Sokol <mareksokol98@gmail.com>
2020-10-18 17:27:45 +0200

    Update cART.m

--- cART.m
+++ cART.m
@@ -20,13 +20,13 @@ angles = 1:step:theta;
 
 for i = 1:niter
     for j = 1:length(angles)
+        ir = imrotate(ir, step, 'bicubic', 'crop');
         tmp = sum(repelem(sg(:, j), 1, length(ir)), 2);
         diff = (tmp - sum(ir, 2)) / length(ir);
         for k = 1:length(ir)
             tdiff(:, k) = diff;
         end
         ir = ir + tdiff;
-        ir = imrotate(ir, step, 'bicubic', 'crop');
     end
 end
 
